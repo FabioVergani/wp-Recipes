e0e40731d59e96961afd6d207e861f3b491ef3b7
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-10-01 12:25:54 +0200

    Create admin_notices_dismisser.php

---
+++ admin_notices_dismisser.php
@@ -0,0 +1,15 @@
+<?php
+//fabio vergani
+add_action('admin_notices',function(){
+?>
+<script>
+window.addEventListener('load',function(){
+	var $=jQuery,f=setTimeout;
+	$('.notice.is-dismissible').each(function(i){
+		var e=$(this);
+		f(function(){e.find('button.notice-dismiss').click();},(i+1)*900);
+	});
+});
+</script>
+<?php
+});
